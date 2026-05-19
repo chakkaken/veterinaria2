ace95c863869a9c5de8256d44af2a1f993a84c10
kelvin <nofearken33@gmail.com>
2026-05-18 20:10:32 -0400

    para seenode

--- vet3/vet3/settings.py
+++ vet3/vet3/settings.py
@@ -38,7 +38,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DJANGO_DEBUG', 'true') == 'false'
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*').split(',')
 LOGIN_URL = 'login'
