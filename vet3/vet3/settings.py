54c4b6d72d0baf6ab03b1518b59df165bbc7771c
kelvin <nofearken33@gmail.com>
2026-05-24 18:23:42 -0400

    usuario nuevo1.2

--- vet3/vet3/settings.py
+++ vet3/vet3/settings.py
@@ -146,7 +146,11 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
