583cb65b3912759eb7ff376a7fbbc0f00ee6a599
kelvin <nofearken33@gmail.com>
2026-05-26 18:56:06 -0400

    logo nuevo y inputs arreglados

--- vet3/Componentes/forms.py
+++ vet3/Componentes/forms.py
@@ -56,6 +56,14 @@ class UsuarioNuevoForm(UserCreationForm):
     email = forms.EmailField(
         widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Correo electrónico'})
     )
+    password1 = forms.CharField(
+        label='Contraseña',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Contraseña'})
+    )
+    password2 = forms.CharField(
+        label='Confirmar contraseña',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirmar contraseña'})
+    )
 
     class Meta:
         model = Usuario
