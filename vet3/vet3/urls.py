7a55fea0c797d23bbc25371e989f5c35a1417e7a
kelvin <nofearken33@gmail.com>
2026-05-27 20:09:52 -0400

    reparar1.4

--- vet3/vet3/urls.py
+++ vet3/vet3/urls.py
@@ -2,11 +2,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   
-    path('', include('vet3.Componentes.Urls')),  # Incluye las URLs de tu aplicación
-     
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('vet3.Componentes.Urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
