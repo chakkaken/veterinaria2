8c6e89948d2fdba6f5b2c0d5b7d5c1a07803f89c
kelvin <nofearken33@gmail.com>
2026-05-29 21:02:55 -0400

    reparar.2.7

--- vet3/vet3/settings.py
+++ vet3/vet3/settings.py
@@ -48,6 +48,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
+SECURE_SSL_REDIRECT = True
 
 
 # Application definition
