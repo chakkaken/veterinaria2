2e05738bbdf7f558b7225b37e8807c41548b9cc9
kelvin <nofearken33@gmail.com>
2026-05-29 20:19:52 -0400

    reparar2.4

--- vet3/Componentes/migrations/0002_fix_usuario_table.py
+++ vet3/Componentes/migrations/0002_fix_usuario_table.py
@@ -10,6 +10,6 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterModelTable(
             name='usuario',
-            table='Componentes_usuario',
+            table='componentes_usuario',
         ),
     ]
